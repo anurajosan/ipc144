27b0c7ce928f531b6559f187bb6e94a7a9ea704a
Anuraj Singh osan <osananuraj841@gmail.com>
2024-05-24 22:55:38 -0400

    test

--- WS01/w1p1.c
+++ WS01/w1p1.c
@@ -1,9 +1,4 @@
 /*
-***************************************************************************
-**
-<assessment name example: Workshop - #1 (Part-1)>
-Full Name : Anuraj Singh Osan
-Student ID#:165546235
 Email :asosan@myseneca.ca
 Section :NBB
 Authenticity Declaration:
